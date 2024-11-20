e63d20521b5e9ec95ed45780c439de93b73a856f
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-11-20 10:45:52 -0800

    Pracitce writing multiple html elements

---
+++ src/Example.jsx
@@ -0,0 +1,15 @@
+const Example = () => {
+  return (
+  <>
+    <h1>Test title</h1>
+    <svg>
+      <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" height="200" width="200"/>
+    </svg>
+    <form>
+      <input type="text" />
+    </form>
+  </>
+  );
+}
+
+export default Example;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Example from './Example.jsx'
 import Greeting from './Greeting.jsx'
 import { Biscuits } from './Biscuits.jsx'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Greeting />
-    <Biscuits />
+    <Example />
   </StrictMode>,
-)
+);
